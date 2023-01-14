0108eb198da3052c824add77b222e31e4f953651
Shaun Lawrence <shaunrlawrence@gmail.com>
2023-01-14 13:53:00 +0000

    Add extension method for registration

---
+++ Popups/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Maui.Views;
+using System.ComponentModel;
+
+namespace Popups;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection RegisterPopup<TPopupView, TPopupViewModel>(this IServiceCollection services)
+        where TPopupView : Popup
+        where TPopupViewModel : INotifyPropertyChanged
+    {
+        PopupService.RegisterPopup<TPopupView, TPopupViewModel>(services);
+
+        return services;
+    }
+}
